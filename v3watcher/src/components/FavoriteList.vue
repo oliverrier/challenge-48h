d85cb040f483c8e2bec742814a50284e12e34425
Olivier <olivier.verrier@ynov.com>
2020-05-14 16:22:42 +0200

    Début vue favoris

---
+++ v3watcher/src/components/FavoriteList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2> Favorite List </h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+  },
+  computed: {
+    favoriteList() {
+      return this.$store.state.favoriteList;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~Style/components/favoriteList';
+</style>
