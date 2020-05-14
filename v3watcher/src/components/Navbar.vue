2ae8af3a8e57eb1bee99808e192fa7dffa7b9ffb
Olivier <olivier.verrier@ynov.com>
2020-05-14 12:41:30 +0200

    NavBar créée

---
+++ v3watcher/src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-div-logo">
+      <img src="@/assets/logoV3.png" />
+      <router-link to="/" class="font-semibold text-xl tracking-tight ml-4">V3 Watcher</router-link>
+    </div>
+    <div class="block lg:hidden" @click="isExpanded=!isExpanded">
+      <button class="navbar-menu-button">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto p-6" v-if="isExpanded">
+      <router-link to="/map" class="navbar-map-button" >Map</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data: () => ({
+    isExpanded: false,
+  }),
+  created() {
+    this.handleResize()
+    window.addEventListener('resize', this.handleResize)
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize)
+  },
+  methods: {
+    handleResize() {
+      if (window.innerWidth >= 1024) this.isExpanded = true;
+      else this.isExpanded = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~Style/components/navbar';
+</style>
---
+++ v3watcher/src/router.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("./components/Home"),
+    },
+    {
+      path: "/map",
+      name: "map",
+      component: () => import("./components/Map"),
+    },
+  ]
+});
