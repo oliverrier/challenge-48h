2f778fd5e742654c228538eea0432ddf56f74265
Mathieu <mathieu.caselles@ynov.com>
2020-05-14 16:02:58 +0200

    ajout systeme favoris

--- v3watcher/src/App.vue
+++ v3watcher/src/App.vue
@@ -2,22 +2,23 @@
   <div id="app">
     <Navbar />
     <div class="pt-32 flex w-full justify-center">
-      <router-view></router-view>
+      <router-view />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Navbar: () => import('@/components/Navbar.vue'),
+    Navbar: () => import("@/components/Navbar.vue")
   }
-}
+};
 </script>
 
 <style lang="scss">
-  html, body {
+html,
+body {
   @apply w-full h-full;
-  }
+}
 </style>
