f8356ffb7d25897926ce9b6db8a5c8abdb57b87c
Mathieu <mathieu.caselles@ynov.com>
2020-05-15 00:00:14 +0200

    ENCORE PLUS PROPRE LE CODE OULALA

--- v3watcher/src/store/index.js
+++ v3watcher/src/store/index.js
@@ -33,7 +33,7 @@ export default new Vuex.Store({
       state.favoriteList = [];
     },
     deleteFavorite(state, val) {
-      const index = state.favoriteList.map(function (station) { return station.id; }).indexOf(val.id);
+      const index = state.favoriteList.findIndex(station => station.hello === val.id);
       state.favoriteList.splice(index, 1);
     },
     updateStationSelect(state, val) {
