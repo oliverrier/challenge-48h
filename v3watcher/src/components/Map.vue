f012c41f5fa7ae937c8b0094b4c152cec724a7aa
Antoine.D <antoine.durand@ynov.com>
2020-05-14 15:10:54 +0200

    premier test map

--- v3watcher/src/components/Map.vue
+++ v3watcher/src/components/Map.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    
+    <div id="map"></div>
   </div>
 </template>
 
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD4sJT9QqmWldPRlgemuuMoUzXLVf81pZg&callback=initMap" async defer></script>
 <script>
 export default {
+  data: () => ({
+    map: null,
+  }),
+  methods: {
+    initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      });
+    }
+  }
 
 }
 </script>
 
-<style>
 
+<style>
+/* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        background-color: aqua;
+        width: 100%;
+        height: 100%;
+      }
 </style>
