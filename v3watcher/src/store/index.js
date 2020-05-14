9c30163bc9c9e18ea7ba6134ce2532eac10370d5
calvin <calvinseaphanh@gmail.com>
2020-05-14 13:17:36 +0200

    changement main

--- v3watcher/src/store/index.js
+++ v3watcher/src/store/index.js
@@ -3,13 +3,40 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+<<<<<<< Updated upstream
 export default new Vuex.Store({
   state: {
+=======
+const axios = require('axios');
+
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex', // The key to store the state on in the storage provider.
+  storage: window.localStorage,
+  reducer: state => ({
+    listeStations: state.listeStations,
+  })
+})
+
+export default new Vuex.Store({
+  state: {
+    listeStations: null,
+
+>>>>>>> Stashed changes
   },
   mutations: {
+    async updateStations(state) {
+      let dataStations = await axios.get(`https://api.alexandredubois.com/vcub-backend/vcub.php`);
+      state.listeStations = dataStations.data;
+    }
   },
   actions: {
   },
   modules: {
+<<<<<<< Updated upstream
   }
 })
+=======
+  },
+  plugins: [vuexLocalStorage.plugin]
+})
+>>>>>>> Stashed changes
