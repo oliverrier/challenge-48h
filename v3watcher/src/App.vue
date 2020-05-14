935f07e7b979d4ea15863e9caf8f0fd7674d2ee8
Mathieu <mathieu.caselles@ynov.com>
2020-05-14 12:47:48 +0200

    ajout des station via requete api

--- v3watcher/src/App.vue
+++ v3watcher/src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logoV3.png">
+    <img alt="Vue logo" src="./assets/logoV3.png" />
+    <button @click="monTest">test</button>
     <Map />
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Map: () => import('@/components/Map'),
+    Map: () => import("@/components/Map")
+  },
+  created() {
+    this.$store.commit("updateStations");
+  },
+  computed: {
+    listeStations() {
+      return this.$store.state.listeStations;
+    }
+  },
+  methods: {
+    monTest: function() {
+      console.log(this.listeStations);
+    }
   }
-}
+};
 </script>
--- v3watcher/src/store/index.js
+++ v3watcher/src/store/index.js
@@ -10,15 +10,19 @@ const vuexLocalStorage = new VuexPersist({
   key: 'vuex', // The key to store the state on in the storage provider.
   storage: window.localStorage,
   reducer: state => ({
-    monStateQueJeVeuxRendrePersistant: state.monStateQueJeVeuxRendrePersistant,
+    listeStations: state.listeStations,
   })
 })
 
 export default new Vuex.Store({
   state: {
-    monStateQueJeVeuxRendrePersistant: null,
+    listeStations: null,
   },
   mutations: {
+    async updateStations(state) {
+      let dataStations = await axios.get(`https://api.alexandredubois.com/vcub-backend/vcub.php`);
+      state.listeStations = dataStations.data;
+    }
   },
   actions: {
   },
